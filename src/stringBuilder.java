6a13a2247615ef90c8efeb4e959253e887139277
akshat <pankajkumar22411@gmail.com>
2023-12-30 22:23:30 +0530

    Updated

---
+++ src/stringBuilder.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class stringBuilder {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    StringBuilder sb = new StringBuilder("Tony");
+    System.out.println(sb);
+
+    System.out.println(sb.charAt(0));
+
+    sb.setCharAt(0, 'P');
+    System.out.println(sb);
+
+    sb.insert(0,'S');
+    System.out.println(sb);
+
+    sb.delete(2,3);
+    System.out.println(sb);
+
+    sb.append("hello");
+    System.out.println(sb);
+  }
+}
