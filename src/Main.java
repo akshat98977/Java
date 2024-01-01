b0589f7f7688d18bbda3a01e801997eff8353dec
akshat <pankajkumar22411@gmail.com>
2024-01-01 23:28:29 +0530

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,54 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-  public static void subsequences(String str, int idx , String newString) {
-    if (idx == str.length()) {
-      System.out.println(newString);
-      return;
+  public static int[] map1 = new int[26];
+  public static int[] map2 = new int[26];
+  static boolean isAnagram(String a, String b) {
+    for(int i = 0; i < a.length(); i++){
+      if(a.charAt(i) - 'a' >= 0){
+        map1[a.charAt(i) - 'a']++;
+      }
+      else if(a.charAt(i) - 'A' >= 0){
+        map1[a.charAt(i) - 'A']++;
+      }
+
     }
-    char currChar = str.charAt(idx);
+    for(int i = 0; i < b.length(); i++){
+      if(a.charAt(i) - 'a' >= 0){
+        map2[a.charAt(i) - 'a']++;
+      }
+      else if(a.charAt(i) - 'A' >= 0){
+        map2[a.charAt(i) - 'A']++;
+      }
 
-    //to be
-    subsequences(str, idx + 1, newString + currChar);
-    //Not to Be
-    subsequences(str, idx + 1, newString);
-
+    }
+    for(int i = 0; i < 26; i++){
+      if(map1[i] != map2[i]){
+        return false;
+      }
+    }
+    System.out.println(map1[19] + " " + map2[19]);
+    return true;
   }
 
-  public static void main (String args[]) {
-    String str = "abc";
-    HashSet<String> set = new HashSet<>();
-    subsequences(str, 0, "");
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    for(int i = 0; i < num; i++){
+      int n = 1;
+      for(int j= 0; j < num; j++){
+        if(i == 0 || j == 0 || i == num-1 || j == num-1){
+          System.out.print(n);
+          n++;
+        }
+
+      }
+      System.out.println();
+    }
   }
-
-
 }
-
-
-
-
-
 
 
 
